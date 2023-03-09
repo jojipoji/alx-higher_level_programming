dd16880b00052a51848f7475292f5dea427ca6e2
jojipoji <jojeepoji@gmail.com>
2023-03-09 10:18:06 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end="")
-    if i != 99:
-       print(", ", end="")
-print()
+for number in range(0, 99):
+    print("{:02d}".format(number), end=", ")
+print("{:02d}".format(number + 1))
