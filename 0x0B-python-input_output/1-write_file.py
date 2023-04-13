e11e5885077039ef397c1eda683b24869ac9d31e
jojipoji <jojeepoji@gmail.com>
2023-04-13 20:52:37 +0300

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write to a file
+Contains the write_file method.
+    """
+
+
+def write_file(filename="", text=""):
+    """ Returns the number of characters written """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
