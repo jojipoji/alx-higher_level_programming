543a260b83ef9de7008e896448bc9be9663c811d
jojipoji <jojeepoji@gmail.com>
2023-04-13 21:50:40 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ a script adding arguments to a Python list,saves them to a file"""
+    """ Really does it"""
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
